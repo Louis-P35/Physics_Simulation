8b28bea0d522819e784eb2197c776022ae343266
Louis-P35 <louis.polart@hotmail.fr>
2025-01-05 23:01:44 +0100

    Clean code from template

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 // Includes from project
 #include "main.hpp"
-#include "StringUtils.hpp"
 #include "mainWindow.hpp"
 
 // Includes from 3rd party
@@ -12,50 +11,10 @@
 #include <string>
 #include <tuple>
 
-void test()
-{
-    // Tests unitaires (vector de tuple de string)
-    std::vector<std::tuple<std::string, std::string, bool>> unitTests = {
-        std::make_tuple("Hello, World!", "Hello, World!", true),
-        std::make_tuple("Hello, World!", "Hello, World!*", true),
-        std::make_tuple("Hello, World!", "*Hello, World!", true),
-        std::make_tuple("Hello, World!", "Hello,*World!", true),
-        std::make_tuple("Hello, World!", "Hello, *World!", true),
-        std::make_tuple("Hello, World!", "*H**Wo*ld!**", true),
-        std::make_tuple("Hello, World!", "S**Wo*ld!**", false),
-        std::make_tuple("Hello, World!", "H**Wo*ld!**S", false),
-        std::make_tuple("Hello, World!", "Hello,World!", false),
-        std::make_tuple("Hello, World!", "Hello,World!*", false),
-        std::make_tuple("Hello, World!", "Hel*l*lo, World!", false),
-        std::make_tuple("Hello, World!", "Rien a voir", false),
-    };
 
-    bool testSucceed = true;
-    for (const auto& test : unitTests)
-    {
-        const std::string& str = std::get<0>(test);
-        const std::string& pattern = std::get<1>(test);
-        const bool res = std::get<2>(test);
-
-        const bool isMatch = StringUtils::match(str, pattern);
-
-        if (isMatch != res)
-        {
-            std::cout << "Test failed: " << str << " ; " << pattern << " ; " << isMatch << " ; " << res << std::endl;
-            testSucceed = false;
-        }
-    }
-
-    if (testSucceed)
-    {
-        std::cout << "All tests succeed!" << std::endl;
-    }
-}
 
 int main(int argc, char** argv)
 {
-    //test();
-
     QApplication app(argc, argv);
     MainWindow window;
     window.show();
--- src/mainWindow.cpp
+++ src/mainWindow.cpp
@@ -1,6 +1,5 @@
 // Includes from project
 #include "mainWindow.hpp"
-#include "StringUtils.hpp"
 
 // Includes from 3rd party
 #include <QMenuBar>
@@ -17,7 +16,7 @@
 MainWindow::MainWindow(QWidget* pParent) : QMainWindow(pParent)
 {
     // Set title
-	setWindowTitle("Qt6 Template Application");
+	setWindowTitle("Physics simulation");
 
     // Set a minimum size for the window
     setMinimumSize(800, 600);
@@ -32,46 +31,4 @@ MainWindow::MainWindow(QWidget* pParent) : QMainWindow(pParent)
 	// Central widget
     QWidget* pCentralWidget = new QWidget(this);
     setCentralWidget(pCentralWidget);
-
-    // Create a vertical layout for the central widget
-    QVBoxLayout* pMainLayout = new QVBoxLayout(pCentralWidget);
-
-    // First field
-    QHBoxLayout* pFirstFieldLayout = new QHBoxLayout;
-    QLabel* pFirstLabel = new QLabel("Enter the string to look into:", this);
-    QLineEdit* pFirstLineEdit = new QLineEdit(this);
-    pFirstFieldLayout->addWidget(pFirstLabel);
-    pFirstFieldLayout->addWidget(pFirstLineEdit);
-
-    // Second field
-    QHBoxLayout* pSecondFieldLayout = new QHBoxLayout;
-    QLabel* pSecondLabel = new QLabel("Enter the string to look for (with *):", this);
-    QLineEdit* pSecondLineEdit = new QLineEdit(this);
-    pSecondFieldLayout->addWidget(pSecondLabel);
-    pSecondFieldLayout->addWidget(pSecondLineEdit);
-
-    // Add the field layouts to the main layout
-    pMainLayout->addLayout(pFirstFieldLayout);
-    pMainLayout->addLayout(pSecondFieldLayout);
-
-	// Add a button to check the pattern
-	QPushButton* pButton = new QPushButton("Check", this);
-	// Add the button to the main layout
-	pMainLayout->addWidget(pButton);
-
-	// Connect the button to the slot, define it on the fly (lambda)
-	connect(pButton, &QPushButton::clicked, [pFirstLineEdit, pSecondLineEdit]()
-		{
-			const std::string str = pFirstLineEdit->text().toStdString();
-			const std::string pattern = pSecondLineEdit->text().toStdString();
-			const bool isMatch = StringUtils::match(str, pattern);
-			if (isMatch)
-			{
-				QMessageBox::information(nullptr, "Match", "The pattern matches the string");
-			}
-			else
-			{
-				QMessageBox::information(nullptr, "No match", "The pattern does not match the string");
-			}
-		});
 }
